8d176c87ca3010919073dd24238a3c94d9fdc2a7
l1ufe1 <l1ufei@163.com>
2023-04-14 22:09:29 +0800

    feat: 添加路由跳转拦截

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+import routerBeforeEach from "./utils/routerBeforeEach";
 onLaunch(() => {
   console.log("App Launch");
+  routerBeforeEach();
 });
 onShow(() => {
   console.log("App Show");
---
+++ src/utils/routerBeforeEach.ts
@@ -0,0 +1,41 @@
+import { getCookieSync } from "./auth";
+
+/** 路由白名单 */
+const whiteList = ["/", "/pages/login/index"];
+
+export default async function () {
+  // 为,uni.navigateTo,uni.redirectTo,uni.reLaunch,uni.switchTab这4个路由方法添加拦截器
+  const list = ["navigateTo", "redirectTo", "reLaunch", "switchTab"];
+  list.forEach((item) => {
+    uni.addInterceptor(item, {
+      invoke(e) {
+        // 获取要跳转的页面路径（url去掉"?"和"?"后的参数）
+        const url = e.url.split("?")[0];
+        // 判断当前窗口是白名单，如果是则不重定向路由
+        let pass;
+        if (whiteList) {
+          pass = whiteList.some((item) => {
+            return url === item;
+          });
+        }
+
+        // 不是白名单并且没有cookie
+        if (!pass && !getCookieSync()) {
+          uni.showToast({
+            title: "请先登录",
+            icon: "none",
+          });
+          uni.navigateTo({
+            url: "/pages/login/index",
+          });
+          return false;
+        }
+        return e;
+      },
+      fail(err) {
+        // 失败回调拦截
+        console.log(err);
+      },
+    });
+  });
+}
