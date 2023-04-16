e9e9107aa162501b9d9f2621faa99c3cdb615d49
l1ufe1 <l1ufei@163.com>
2023-04-16 10:19:45 +0800

    fix: 去除强制去首页

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,7 @@ onLaunch(async () => {
   // 注册路由守卫
   routerBeforeEach();
   const useUserApi = useUser();
-  if (await useUserApi.verifyLoginStatus()) {
-    // 登录有效，跳转到首页
-    uni.redirectTo({
-      url: "/pages/home/index",
-    });
-  }
+  await useUserApi.verifyLoginStatus();
 });
 onShow(() => {
   console.log("App Show");
