d29f18612a4632119d206f4fafac1495a72e28e7
l1ufe1 <l1ufei@163.com>
2023-04-24 21:27:21 +0800

    feat: 添加pinia

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createSSRApp } from "vue";
+import * as Pinia from "pinia";
 import App from "./App.vue";
-import '@/style/iconfont.css'
-import Layout from '@/components/layout/index.vue'
+import "@/style/iconfont.css";
+import Layout from "@/components/layout/index.vue";
 
 export function createApp() {
   const app = createSSRApp(App);
-  app.component('Layout', Layout)
+  app.component("Layout", Layout)
+  app.use(Pinia.createPinia());
   return {
     app,
+    Pinia,
   };
 }
